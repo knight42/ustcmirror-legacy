7219780052001cd909cf1fe6041927d47d4b8fda
Knight <knight42@mail.ustc.edu.cn>
2016-07-24 16:52:28 +0800

    specify name

--- bringup.sh
+++ bringup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 user="${2:-$USER}"
-docker run --rm -v "$1":/var/spool/apt-mirror -u "$(id -u $user):$(id -g $user)" ustclug/apt-dockerproject-mirror:latest
+docker run --rm -v "$1":/var/spool/apt-mirror --name 'syncing-apt.dockerproject.org' -u "$(id -u $user):$(id -g $user)" ustclug/apt-dockerproject-mirror:latest
