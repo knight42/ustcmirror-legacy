7855fe1b7a90b5cb1761637f647c6b93db2e7850
knight42 <anonymousknight96@gmail.com>
2017-01-06 01:22:16 +0800

    Simplify bringup.sh

--- bringup.sh
+++ bringup.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-user="${2:-$USER}"
-docker run --rm -v "$1":/var/spool/apt-mirror --name 'syncing-apt.dockerproject.org' -u "$(id -u $user):$(id -g $user)" ustclug/apt-dockerproject-mirror:latest
+uid="$(stat -c '%u' "$1")"
+gid="$(stat -c '%g' "$1")"
+name='apt.dockerproject.org'
+docker run --rm --name "syncing-$name" --label=syncing \
+    -e "uid=$uid" -e "gid=$gid" \
+    -v "$1":/var/spool/apt-mirror/mirror/$name \
+    -v "$2":/var/spool/apt-mirror/var \
+    ustclug/apt-dockerproject-mirror:latest
